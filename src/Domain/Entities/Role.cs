94f0d64a96e4bdf998a5662db22dc8b3e6628ad2
Dikshya Dhakal <dikshya@codebee.dk>
2025-05-01 09:07:16 +0545

    initial commit

---
+++ src/Domain/Entities/Role.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Domain.Entities;
+
+public class Role
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public List<UserRole> UserRoles { get; set; }
+
+}
---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Domain.Entities;
+
+public class User
+{
+    public int ID { get; set; }
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+
+    public List<UserRole> UserRoles { get; set; }
+    public List<Blog> Blogs { get; set; }
+    public List<Comment> Comments { get; set; }
+
+}
